99218170f57449fb930cab1b9de55bb4d14d8881
Suraj kumar <sooraj4475@gmail.com>
2024-04-19 06:07:59 +0000

    DataTypes

---
+++ Basic/DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  //we are using node js not browser
+
+console.log(3
+    +3) // we cannot write code like this , code readibility should be high
+console.log("suraj")
+
+let name="suraj" // string
+let age =22 //number
+let isLoggedIn=false //boolean/ false/true
+let state;
+//null=> standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "suraj"); //string
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
