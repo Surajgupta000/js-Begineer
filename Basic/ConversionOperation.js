2edc744ba6d87fc2083e2154eb2bd4e32236489a
Suraj kumar <sooraj4475@gmail.com>
2024-04-19 06:32:17 +0000

    conversionOfdatatypes

---
+++ Basic/ConversionOperation.js
@@ -0,0 +1,24 @@
+let score="33aab" //NaN not-a- number
+// console.log(typeof score);
+// console.log(typeof (score)); //as a mrthod we can also check its datatype by keeping it in small brackets
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=> NaN
+// true=>1
+//false=>0
+
+let isLoggedIn= 1
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//1=>true; 0=> false
+//""=>false
+//"suraj"=>true
+
+let somenum=33
+let stringnmu= String(somenum)
+console.log( typeof stringnmu);
