8be3244987bbc822c9d2251098d7fd94bd63c553
Suraj kumar <sooraj4475@gmail.com>
2024-04-19 06:09:51 +0000

    Variables

---
+++ Basic/variables.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmial="sooraj123@google.com"
+var accountPassword="0000"
+accountCity="bhopal"
+let accountState;
+
+// accountId=3 
+
+
+accountEmial="hc@jdfg"
+accountPassword="123"
+accountCity="lsjh"
+console.log(accountId);
+console.table([accountId, accountEmial, accountPassword, accountCity, accountState])
