0de401a5e939d787bb30d2c794aa9762c9332b0a
Suraj kumar <sooraj4475@gmail.com>
2024-04-20 07:29:08 +0000

    datatype summary

---
+++ Basic/datatype.js
@@ -0,0 +1,15 @@
+//data types are categorsied by means of storing and accessing the data by which we can difference them in primitive and non primitive
+//primitive datatypes: call by value 
+
+//7 types: when we access the primitive datatypes we get the copy of the datatypes and any changes done only in the copy
+// string, number, boolean, null(pura khali), undefined( under value nhi h), symbol, BigInt
+
+// Dynamically-typed languages are those (like JavaScript) where the interpreter assigns variables a type at runtime based on the variable's value at the time.
+// refernce type(non primitive)
+
+//Arrays, objects, (to master js then master the object in js and web events in js), functions
+
+const id= Symbol('123')
+const anotherid= Symbol('123')
+console.log(id=== anotherid);
+
