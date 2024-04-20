06b398cda61fd6b06feee3397a21609737f3f284
Suraj kumar <sooraj4475@gmail.com>
2024-04-20 07:05:37 +0000

    conversions

---
+++ Basic/Conversions.js
@@ -0,0 +1,5 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log("2"===2); // === checks datatype also and == check if equal or not and convert it to num.
