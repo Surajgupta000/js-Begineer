2cdad4d6590b89bfb927cb3541c996dd33791aed
Suraj kumar <sooraj4475@gmail.com>
2024-04-21 17:26:28 +0000

    datatypes

--- Basic/datatype.js
+++ Basic/datatype.js
@@ -1,4 +1,5 @@
 //data types are categorsied by means of storing and accessing the data by which we can difference them in primitive and non primitive
+
 //primitive datatypes: call by value 
 
 //7 types: when we access the primitive datatypes we get the copy of the datatypes and any changes done only in the copy
@@ -13,3 +14,23 @@ const id= Symbol('123')
 const anotherid= Symbol('123')
 console.log(id=== anotherid);
 
+
+// ***********************
+// MEMORY
+//stack(primitive) and we get the copy of the memory _____, Heap(Non-primitve) we get refernce of oroginal value
+
+let test1= "suraj"
+let test2= test1
+
+test2= "kumar"
+console.log(test1);
+console.log(test2);
+
+let user={
+    email:"sooraj@gmail.com",
+    upi:"1234"
+}
+let user2= user
+user2.email="sk@sj"
+console.log(user.email);
+console.log(user2.email);
