21217576d496946c511882d7c9d99f694c3286df
Suraj kumar <sooraj4475@gmail.com>
2024-04-22 05:32:47 +0000

    string

---
+++ Basic/string.js
@@ -0,0 +1,20 @@
+// const name= "suraj"
+// const repo= 50
+
+// // console.log(name + repo + "kumar"); we cant use this method for concatenate strings in console. instead of this we can use modern method to write the same which is more effeicent and more readable.
+
+// console.log( ` Hello my name is ${name.toUpperCase() } and my repo count is ${repo}`); //`` is called backslacsh
+
+
+// const gamename= new String('surajkr') // 2 method to define string with new keyword in which we are accessing the object of string and surajkr is constructor
+
+// console.log(gamename[0]); // by this [0] we can get the 0 no element like an array but this is not an array
+
+
+// console.log(gamename.length);
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('r'));
+
+const url ="https://surajkr.com"
+console.log(url.replace('kr', 'gupta'));
+console.log(url.includes('kg'));
