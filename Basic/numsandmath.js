4adfee7d986fae1dba059fdde30a704e14df7e7e
Suraj kumar <sooraj4475@gmail.com>
2024-04-22 07:02:15 +0000

    MathAndnums

---
+++ Basic/numsandmath.js
@@ -0,0 +1,30 @@
+const score= 300
+// console.log(score);
+
+const bal= new Number(200)
+// console.log(bal);
+
+// console.log(bal.toString());
+// console.log(bal.toFixed(2));
+
+const num= 1000000
+// console.log(num.toLocaleString('en-IN'));
+
+// ********************************maths
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.7));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max= 20
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
+
+
